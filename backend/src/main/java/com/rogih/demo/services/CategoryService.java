82e6a198fe5c0755fa9d3e3c60a330a6ef83f65c
HigorSouza <higor.andrade@hotmail.com>
2022-10-09 14:46:46 -0300

    Criação de DTO

--- backend/src/main/java/com/rogih/demo/services/CategoryService.java
+++ backend/src/main/java/com/rogih/demo/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.rogih.demo.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.rogih.demo.dto.CategoryDTO;
 import com.rogih.demo.entities.Category;
 import com.rogih.demo.repositories.CategoryRepository;
 
@@ -16,8 +18,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
 }
